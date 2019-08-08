e60648e9627097d2e235f1bd728cac9f9cf56339
icarus <ellishogan95@gmail.com>
2019-08-08 15:17:15 -0400

    dots

--- src/dots.js
+++ src/dots.js
@@ -1,36 +1,4 @@
-const addArrClass = (e, c) => { //e is element and c is classes to add
-  for (let i = 0; i < c.length; i++) {
-    i != 0 ? e.setAttributeNS(null, "class", " "): null; //doesnt add a space to the first element for class name seperation
-    e.setAttributeNS(null, "class", c[i]); //add the classes
-  }
-}
-
-const makeSVG = (el, howMany, classSVG, classElement, w, h, apnd) => {
-  let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  svg.setAttributeNS(null, 'width', w);
-  svg.setAttributeNS(null, 'height', h);
-  //checks to see if the cssSVG comes in as a string and adds the class if so. if not it adds all the classes in the array
-  typeof classSVG === "string" ?  svg.setAttributeNS(null, "class", classSVG) : addArrClass(svg, classSVG);
-  // default svg animation builder
-  switch (el){
-
-
-    default:
-      let dot = []
-      for (let i = 0; i < howMany; i++){
-        let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-        dot.push(circle);
-        dot[i].className += classElement[i % classElement.length];
-        dot[i].setAttributeNS(null, 'cy', -5);
-        dot[i].setAttributeNS(null, 'cx', w - 15);
-        dot[i].setAttributeNS(null, 'r', 5);
-        svg.appendChild(dot[i]);
-        dot[i].className +=  ` dot dotNo${i}`;
-      }
-  }
-  return document.querySelector(apnd).appendChild(svg);
-}
-
+import anime from '../anime/lib/anime.es.js';  
 
 const where = (e) => {
   return (e[1] + e[0])/2 ;
@@ -292,7 +260,7 @@ let animation = (e, t, svg, mvX, mvY, crv, clr, d) => {
   
 }
 
-let dotsMovement = (svg, mvX, mvY, t, crv, clr) => {
+export let dotsMovement = (svg, mvX, mvY, t, crv, clr) => {
   let delay = (el,time, n) =>{
     return 3.5 * time / el.children.length * n
   }
@@ -312,14 +280,4 @@ let dotsMovement = (svg, mvX, mvY, t, crv, clr) => {
   }
 }
 
-
-
-
-
-
-
-let dotsLTR = makeSVG("heartBeat", 4, "dot", ["dot-c1", "dot-c2"], 500, 360, ".stage");
-let dotsRTL = makeSVG("heartBeat", 7, "dot2", ["dot-c1", "dot-c2"], 500, 360, ".stage");
-dotsMovement(dotsLTR, [-40, -360], [10, 360], 2300, "cubicX", "#F30659");
-dotsMovement(dotsRTL, [-360, -40], [10, 360], 1000, "cubicX", "#F30659");
 
