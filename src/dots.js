9a3e0cd23f9f40447c19cf036859a528b0005ddf
icarus <ellishogan95@gmail.com>
2019-08-14 22:17:47 -0400

    some lost some gained"

--- src/dots.js
+++ src/dots.js
@@ -220,38 +220,38 @@
           
     
   }
-  let howFarX = (s, e, el) => {
-    console.log(el)
+  let whereStart = (x, y, el) => {
+    console.log(s)
     let start = document.getElementById(s).getBoundingClientRect()
     let end = document.getElementById(e).getBoundingClientRect()
     let element = el.getBoundingClientRect()
     let uv = (element.left+element.right)/2
-    let x = [(start.left+start.right)/2 -uv, (end.left+end.right)/2 -uv];
+    x = [(start.left+start.right)/2 -uv, (end.left+end.right)/2 -uv];
     console.log(uv)
     return x
   }
-  let howFarY = (s, e, el) => {
+  let whereEnd = (x, y, el) => {
     let start = document.getElementById(s).getBoundingClientRect()
     let end = document.getElementById(e).getBoundingClientRect()
     let element = el.getBoundingClientRect()
     let uh = (element.top+element.bottom)/2
     let st = (start.top+start.bottom)/2
     console.log(st, uh, element)
-    let y = [st -uh, (end.top+end.bottom)/2 -uh];
+    y = [st -uh, (end.top+end.bottom)/2 -uh];
     return y
   }
-  let animation = (e, t, svg, start, end, crv, clr, d) => {
+  let animation = (e, t, svg, mvX, mvY, crv, clr, d) => {
 
       let animate = anime.timeline({
   
       }).add({
           targets: svg.children[e],         
-          translateX: translateX(start, t, crv),
-          translateY: translateY(end, t, crv),
+          translateX: translateX(mvX, t, crv),
+          translateY: translateY(mvY, t, crv),
           fill: fillColor(clr, t, svg),
           backgroundColor: fillColor(clr, t, svg),
           complete: function(anim){
-            animation(e, t, svg, start, end, crv, clr, 0)
+            animation(e, t, svg, mvX, mvY, crv, clr, 0)
           },
       }, d);
     
@@ -259,23 +259,40 @@
     
   }
   
-  var dotsMovement = (svg, start, end, t, crv, clr) => {
+  var dotsMovement = (params) => {
     let delay = (el,time, n) =>{
       return 3.5 * time / el.children.length * n
     }
-    let distX = [start[0], end[0]];
-    let distY = [start[1], end[1]];
+    let distX = [];
+    let distY = [];
+    if (typeof params.start === Object) {
+      let start = whereStart(params.start[0], params.start[1], e.children[i]);
+      distX[0] = start[0];
+      distY[0] = start[1];
+    } else {
+      distX[0] = params.start[0];
+      distY[0] = params.start[1];
+    }
+    if (typeof params.end === Object) {
+      let end = whereEnd(params.end[0], params.end[1], e.children[i]);
+      distX[1] = end[0];
+      distY[1] = end[1];
+    } else {
+      distX[1] = params.end[0];
+      distY[1] = params.end[1];
+    }
+
     //mobile functions for distance
     let loopThrough = (e) => {
       for (let i = 0; i < e.children.length; i++){
-        animation(i, t, e, howFarX(distX[0], distX[1], e.children[i]), howFarY(distY[0], distY[1], e.children[i]), crv, clr, delay(e, t, i))
+        animation(i, params.time, e, distX, distY, params.curve, params.color, delay(e, params.time, i))
       }
     }
-    if (typeof svg.length == 'undefined') {
-      loopThrough(svg)
+    if (typeof params.target.length == 'undefined') {
+      loopThrough(params.target)
     } else {
-      for (let j = 0; j < svg.length; j++){
-        loopThrough(svg[j])
+      for (let j = 0; j < params.target.length; j++){
+        loopThrough(params.target[j])
       }
     }
   }
